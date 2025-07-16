bc350f09fa43bf9c19ae3dc6d9bd3352eb4e2df1
Ana Gonzalez <157073381+anagonzalez94@users.noreply.github.com>
2025-07-16 20:19:39 +0000

    big commit

--- SuperAles.py
+++ SuperAles.py
@@ -1,3 +1,33 @@
+# Create constructor
+class Ale:
+    def __init__(self, name, abv):
+        self.name = name
+        self.abv = abv
+    def alcohol_content(self, volume_in_oz):
+        return self.abv * volume_in_oz
+    def description(self):
+        return f"{self.name}: {self.abv*100:.1f}% ABV"
+# Rewriting individual subclasses using constructor
+class PaleAle(Ale):
+    def __init__(self):
+        super().__init__("PaleAle", 0.055)
+class IPA(Ale):
+    def __init__(self):
+        super().__init__("IPA", 0.065)
+class Stout(Ale):
+    def __init__(self):
+        super().__init__("Stout", 0.07)
+class Porter(Ale):
+    def __init__(self):
+        super().__init__("Porter", 0.06)
+# Testing
+if __name__=="__main__":
+    ales = [PaleAle(),IPA(),Stout(),Porter()]
+    # Iterate through each instance of the subclass in list
+    for ale in ales:
+        # Calls description from each object
+        print(ale.description())
+        print (f"Alcohol content in 16 oz: {ale.alcohol_content(16):.2f} oz.")
 
 
 #--------------------------------------------------------------------------------#
--- SuperVehicles.py
+++ SuperVehicles.py
@@ -1,4 +1,28 @@
+# Creating the constructor
+class Vehicle:
+    def __init__(self, name, mpg):
+        self.name = name
+        self.mpg = mpg
+    def fuel_needed(self, distance):
+        return distance / self.mpg
+    def description(self):
+        return f"{self.name} gets {self.mpg} miles per gallon."
+# Now rewriting the individual subclasses using the constructor "Vehicle"
+class Car(Vehicle):
+    def __init__(self):
+        super().__init__("Car", 30)
 
+class Truck(Vehicle):
+    def __init__(self):
+        super().__init__("Truck", 15)
+
+class Motorcycle(Vehicle):
+    def __init__(self):
+        super().__init__("Motorcycle", 50)
+
+class Bus(Vehicle):
+    def __init__(self):
+        super().__init__("Bus", 8)
 
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
